3cf53905ddbc73becd2c9ad3caea3bf39c74dd3f
dickyzuerst <dzuerst@gmail.com>
2024-10-02 14:12:18 +0700

    menambahkan searchfield dan sizedbox

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,7 +8,68 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      body: Column(
+        children: [
+          _searchField(),
+          const SizedBox(height: 40,),
+        ],
+      ),
     );
+  }
+
+  Container _searchField() {
+    return Container(
+        margin: const EdgeInsets.only(top: 40,left: 20,right: 20),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.11),
+              blurRadius: 10,
+              spreadRadius: 0.0
+            )
+          ]
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: const EdgeInsets.all(15),
+            hintText: 'Search Hadits',
+            hintStyle: const TextStyle(
+              color: Color(0xffDDDADA),
+              fontSize: 14
+            ),
+            prefixIcon: Padding(
+              padding: const EdgeInsets.all(12),
+              child: SvgPicture.asset('assets/icons/search.svg'),
+            ),
+            suffixIcon: Container(
+              width: 100,
+              child: IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const VerticalDivider(
+                      color: Colors.black,
+                      indent: 10,
+                      endIndent: 10,
+                      thickness: 0.1,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SvgPicture.asset('assets/icons/filter.svg'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide.none
+            )
+          ),
+        ),
+      );
   }
 
   AppBar appBar() {
